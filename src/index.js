ab9b6186692fa90b200cd64a99f7a32972942a62
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-09-16 16:01:25 +0300

    Implemented delete, insert

--- src/index.js
+++ src/index.js
@@ -62,25 +62,52 @@ function deleteItem(root, value) {
     while (curr !== null && curr.data !== value) {
         prev = curr;
         if (value > curr.data) curr = curr.right;
-        if (value < curr.data) curr = curr.left;
+        else if (value < curr.data) curr = curr.left;
     }
     if (curr == null) {
         console.log("value is not here!");
         return root;
     }
+
     // if node to be deleted has one child
     if (curr.left === null || curr.right == null) {
         let newCurr = curr.right === null ? curr.left : curr.right;
+
+        // if it's root node
         if (prev === null) {
             return newCurr;
         }
-        if (prev.right === curr) prev.right === newCurr;
-        else if (prev.left === curr) prev.left === newCurr;
+        if (prev.right === curr) prev.right = newCurr;
+        else prev.left = newCurr;
+    } else {
+        // if node to be deleted has two children
+        let prevNextBiggest = null;
+        let nextBiggest = curr.right;
+        // get the nextBiggest
+        while (nextBiggest.left !== null) {
+            prevNextBiggest = nextBiggest;
+            nextBiggest = nextBiggest.left;
+        }
+
+        if (prevNextBiggest === null) {
+            // if it's the root node and the nextBiggest is the first node to the right
+            // then delete the node to the right
+            curr.right = nextBiggest.right;
+        } else {
+            // if it's not the root node
+            // then delete the next biggest
+            prevNextBiggest.left = nextBiggest.right;
+        }
+        // replace the node to be deleted value with nextBiggest value
+        curr.data = nextBiggest.data;
     }
+    return root;
 }
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const tree = new Tree(arr);
 insert(tree.root, 40);
+prettyPrint(tree.root);
 
+deleteItem(tree.root, 4);
 prettyPrint(tree.root);
